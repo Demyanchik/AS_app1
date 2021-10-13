5c432873bf9099962b2542df145dd47ee5cb35d1
Ilya Demyanchik <neyverlin@outlook.com>
2021-10-13 10:36:31 +0300

    c2

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.app1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 //  TODO some comment
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayText();
+
+        for (int j = 0; j < 10; j++) {
+            Log.d("MainActivity", "j = " + j);
+        }
+    }
+
+    private void displayText() {
+        TextView nt = findViewById(R.id.newtest);
+
+        TextFunction tf = new TextFunction();
+        nt.setText(tf.getValue());
     }
 }
